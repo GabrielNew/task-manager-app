7bb05168c4ff8af5fe24f3ef719f87365e2f3ca7
GabrielNew <gabrielfai70@gmail.com>
2025-01-21 10:30:59 -0300

    Update task and user models

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  completed: { type: Boolean, required: true },
+  description: { type: String, required: true, trim: true },
+  completed: { type: Boolean, required: false, default: false },
 });
 
 const Task = new mongoose.model("task", TaskSchema);
--- server/models/User.js
+++ server/models/User.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 import validator from "validator";
 
 const UserSchema = mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, trim: true },
   email: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
     validade(value) {
       if (!validator.isEmail(value)) {
         throw new Error("Email is invalid");
@@ -17,6 +19,17 @@ const UserSchema = mongoose.Schema({
     validade(value) {
       if (value < 0) {
         throw new Error("Age must be a positive number");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 7,
+    validade(value) {
+      if (value.toLowerCase().contains("password")) {
+        throw new Error("This password is too insecure");
       }
     },
   },
