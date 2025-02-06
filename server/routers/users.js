a5de48411dde422388f59a84fd9487d2fece7e24
GabrielNew <gabrielfai70@gmail.com>
2025-02-06 10:02:29 -0300

    Refactoring update and delete users endpoints

--- server/routers/users.js
+++ server/routers/users.js
@@ -61,20 +61,7 @@ userRouter.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-userRouter.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-userRouter.patch("/users/:id", async (req, res) => {
+userRouter.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every((update) =>
@@ -86,8 +73,7 @@ userRouter.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const _id = req.params.id;
-    const user = await User.findById(_id);
+    const user = req.user;
 
     updates.forEach((update) => {
       user[update] = req.body[update];
@@ -95,25 +81,16 @@ userRouter.patch("/users/:id", async (req, res) => {
 
     await user.save();
 
-    if (!user) {
-      return res.status(404).send();
-    }
-
     res.send(user);
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
-userRouter.delete("/users/:id", async (req, res) => {
+userRouter.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      res.status(404).send();
-    }
-
-    res.status(200).send(user);
+    await req.user.deleteOne();
+    res.status(200).send(req.user);
   } catch (error) {
     res.status(500).send(error);
   }
