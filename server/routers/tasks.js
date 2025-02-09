1142065ca425a9979b02b9d74c10992fcaf606ae
GabrielNew <gabrielfai70@gmail.com>
2025-02-08 22:15:37 -0300

    Pagination and sort

--- server/routers/tasks.js
+++ server/routers/tasks.js
@@ -31,18 +31,29 @@ taskRouter.get("/tasks/:id", auth, async (req, res) => {
 
 taskRouter.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split("_");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
 
   try {
     const tasks = await req.user.populate({
       path: "tasks",
       match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
     });
     //const tasks = await Task.find({ owner: req.user._id });
-    res.status(200).send(tasks);
+    res.status(200).send(tasks.tasks);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
